ef933ad6917aec695c3918e61f8a268d95c8792f
Mdmuzaffarimam <muzaffarimam1@yahoo.com>
2026-02-14 00:06:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,56 +1,32 @@
-import asyncio
-from aiohttp import web
-
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN, PORT
-
+from config import API_ID, API_HASH, BOT_TOKEN
 
 class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            "vj join request bot",
-            api_id=API_ID,
-            api_hash=API_HASH,
-            bot_token=BOT_TOKEN,
-            plugins=dict(root="plugins"),
-            workers=50,
-            sleep_threshold=10
+        "vj join request bot",
+         api_id=API_ID,
+         api_hash=API_HASH,
+         bot_token=BOT_TOKEN,
+         plugins=dict(root="plugins"),
+         workers=50,
+         sleep_threshold=10
         )
 
+      
     async def start(self):
-
+            
         await super().start()
         me = await self.get_me()
         self.username = '@' + me.username
-
+            
         print('Bot Started Powered By @VJ_Botz')
 
-        # ✅ Web server start for health check
-        await self.start_web_server()
 
     async def stop(self, *args):
 
         await super().stop()
         print('Bot Stopped Bye')
 
-    # 🌐 Web server function
-    async def start_web_server(self):
-
-        async def root(request):
-            return web.Response(text="Bot Running ✅")
-
-        app = web.Application()
-        app.router.add_get("/", root)
-
-        runner = web.AppRunner(app)
-        await runner.setup()
-
-        site = web.TCPSite(runner, "0.0.0.0", PORT)
-        await site.start()
-
-        print(f"Web Server Started On Port {PORT}")
-
-
-# ▶️ Run Bot
 Bot().run()
