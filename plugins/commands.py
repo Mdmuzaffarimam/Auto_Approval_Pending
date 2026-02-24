efdfa00f8b4d251025a51f73a79a10bf7945a3e9
Mdmuzaffarimam <muzaffarimam1@yahoo.com>
2026-02-24 15:34:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -245,8 +245,7 @@ async def auto_approve(client, m):
             await client.send_message(
                 chat_id=m.from_user.id,
                 text=f"""
-<b>
-<blockquote>Hello {m.from_user.mention}!</blockquote>
+<b><blockquote>Hello {m.from_user.mention}!</blockquote>
 <blockquote>Welcome To {m.chat.title}</blockquote>
 
 <blockquote>Powered By : @Mrn_Officialx</blockquote>
