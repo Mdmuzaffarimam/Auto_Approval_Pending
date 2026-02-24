cf5d7a365c1101092339a782cc91e86cb4f216bb
Mdmuzaffarimam <muzaffarimam1@yahoo.com>
2026-02-24 12:32:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -244,12 +244,12 @@ async def auto_approve(client, m):
         try:
             await client.send_message(
                 chat_id=m.from_user.id,
-                text=f"""
+                text=f"""<b>
 <blockquote>Hello {m.from_user.mention}!</blockquote>
 <blockquote>Welcome To {m.chat.title}</blockquote>
 
 <blockquote>Powered By : @Mrn_Officialx</blockquote>
-""",
+"""</b>,
                 parse_mode=enums.ParseMode.HTML
             )
         except Exception:
